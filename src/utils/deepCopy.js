b12c47697238f1b27fc27adce9e23e505543f6e9
zhaolei <nykee@sina.com>
2020-06-10 13:40:35 +0800

    feat:增加Utils函数节流防抖

--- src/utils/deepCopy.js
+++ src/utils/deepCopy.js
@@ -1,9 +1,8 @@
-
 export function deepCopy(obj1) {
     var obj2 = Array.isArray(obj1) ? [] : {};
     if (obj1 && typeof obj1 === "object") {
         for (var i in obj1) {
-            if (obj1.hasOwnProperty(i)) {
+            if (Object.prototype.hasOwnProperty.call(obj1,i)) {
                 // 如果子属性为引用数据类型，递归复制
                 if (obj1[i] && typeof obj1[i] === "object") {
                     obj2[i] = deepCopy(obj1[i]);
